56f4cda4cb55c74860c86720579668c107e68b18
Bruno Arndt <bruno.arndt@dc.ufscar.br>
2015-12-05 17:13:22 -0200

    Tutorial: Instrucoes para a instalacao dos softwares necessarios para a execucao do experimento

--- tutorial/experimento.php
+++ tutorial/experimento.php
@@ -49,6 +49,12 @@
                                         </a>
                                     </li>
                                     <li>
+                                        <a href="#instalar_linux" id="instalar_linux-tab" role="tab" data-toggle="tab"
+                                            aria-controls="instalar_linux" aria-expanded="false">
+                                            Instalando os Softwares necess&aacute;rios
+                                        </a>
+                                    </li>
+                                    <li>
                                         <a href="#abrir_eclipse" id="abrir_eclipse-tab" role="tab" data-toggle="tab"
                                             aria-controls="abrir_eclipse" aria-expanded="false">
                                             Como abrir as inst&acirc;ncias do Eclipse
@@ -143,6 +149,155 @@
                                                 $contImg = 0;
                                                 include("dados/metodo_conteudo_pacote.php");
                                             ?>
+                                        </div><!-- /container -->
+                                    </div><!-- /container -->
+                                </div>
+                                <div role="tabpanel" class="tab-pane fade" id="instalar_linux" aria-labelledby="instalar_linux-tab">
+                                    <div class="container container_tab">
+                                        <div class="page-header">
+                                            <h1 class="text-center">Instalando os Softwares necess&aacute;rios</h1>
+                                        </div>
+                                        <div class="container container_tab">
+                                            <div class="caption">
+                                                <h3>Sistema Operacional</h3>
+                                            </div>
+                                             <div class="caption">
+                                                <div class="texto_explicativo">
+                                                    <p>
+                                                        Para fazer este experimento, &eacute; preciso utilizar o Linux como Sistema Operacional.
+                                                        Caso voc&ecirc; n&atilde;o utilize Linux em seu computador, voc&ecirc; tem 3 op&ccedil;&otilde;es:
+                                                    </p>
+                                                    <ol>
+                                                        <li>Utilizar as m&aacute;quinas do DC;</li>
+                                                        <li>Utilizar uma m&aacute;quina virtual; e</li>
+                                                        <li>Criar um dual-boot na sua m&aacute;quina.</li>
+                                                    </ol>
+                                                    <p>
+                                                        Caso voc&ecirc; opte pela op&ccedil;&atilde;o <strong>Utilizar uma m&aacute;quina virtual</strong>,
+                                                        basta clicar <a href="<?php echo $urlMaquinaVirtual; ?>" target="_blank">aqui</a>
+                                                        e import&aacute;-la para seu VirtualBox (clique <a href="<?php echo $urlVirtualBox; ?>" target="_blank">aqui</a> para baixar o VirtualBox).
+                                                        O <strong>usu&aacute;rio</strong> desta VM &eacute; <u>experimento</u> e sua <strong>senha</strong> &eacute; <u>teste</u>.
+                                                        J&aacute; o <strong>usu&aacute;rio</strong> do PostgreSQL &eacute; <u>postgres</u> e sua <strong>senha</strong> &eacute; <u>teste</u>.
+                                                        Al&eacute; disso, o conte&uacute;do do experimento est&aacute; em <kbd>/var/www/html/metodo_mdd</kbd>.
+                                                        Nesta op&ccedil;&atilde;o <strong>n&atilde;o precisa</strong>
+                                                        seguir os passos de <strong>Instala&ccedil;&atilde;o e configura&ccedil;&atilde;o do Apache, PHP, MySQL e PostgreSQL</strong>.
+                                                    </p>
+                                                    <p>
+                                                        Caso voc&ecirc; opte pela op&ccedil;&atilde;o <strong>Criar um dual-boot na sua m&aacute;quina</strong>,
+                                                        basta clicar <a href="<?php echo $urlUbuntu1404LTS; ?>" target="_blank">aqui</a> para baixar o <strong>Ubuntu 14.04 LTS</strong>.
+                                                        Ap&oacute;s isso, grave a imagem em um CD e fa&ccedil;a o <i>boot</i> de sua m&aacute;quina pelo CD.
+                                                        Caso voc&ecirc; n&atilde;o tenha experiencia em configurar um Dual-boot,
+                                                        clique <a href="http://www.profissionaisti.com.br/2014/06/dual-boot-instalando-o-windows-e-linux-ubuntu-no-mesmo-pc/" target="_blank">aqui</a>
+                                                        para um tutorial sobre o assunto.
+                                                    </p>
+                                                </div>
+                                            </div>
+                                            <div class="caption">
+                                                <h3>Instalando o Vim, Apache, PHP, MySQL e PostgreSQL</h3>
+                                            </div>
+                                             <div class="caption">
+                                                <div class="texto_explicativo">
+                                                    <p>
+                                                        Para voc&ecirc; instalar o Apache, o PHP, o MySQL e o PostgreSQL basta abrir um Terminal (atalho <kbd>ctrl+alt+T</kbd>) e executar o sequinte comando:
+                                                    </p>
+                                                    <pre>
+                                                        <p>sudo apt-get update</p>
+                                                        <p>sudo apt-get install vim apache2 mysql-server php5-mysql php5 libapache2-mod-php5 php5-mcrypt php5-cgi php5-common php5-curl php-pear postgresql postgresql-contrib pgadmin3</p>
+                                                    </pre>
+                                                    <p>
+                                                        Durante a instala&ccedil;&atilde;o ser&aacute; perguntada a <strong>senha <u>root</u> do MySQL</strong>. Ap&oacute; o t&eacute;rmino da instala&ccedil;&atilde;o,
+                                                        iremos configurar acessar o console do PostgreSQL para configurar a senha do usu&aacute;rio <u>postgres</u>.
+                                                    </p>
+                                                    <pre>
+                                                        <p>sudo -i -u postgres</p>
+                                                        <p>psql</p>
+                                                    </pre>
+                                                    <p>
+                                                        Com isso, podemos executar o seguinte comando SQL para alterar a senha do usu&aacute;rio <u>postgres</u>.
+                                                    </p>
+                                                    <pre>ALTER USER postgres WITH PASSWORD '&lt;newpassword&gt;';</pre>
+                                                    <p>
+                                                        Ap&oacute;s isso, configuramos o pgAdmin3 para acessar o servidor local do PostgreSQL.
+                                                        Basta clicar em <strong>Add a connection to a server.</strong> e preencher os seguintes dados (conforme a Figura <a href="#figura_experimento_pgadmin3_add_server">1</a>):
+                                                    </p>
+                                                    <ul>
+                                                        <li><strong>Name</strong>: Um nome para o servidor;</li>
+                                                        <li><strong>Host</strong>: IP ou DNS do servidor (<strong>padr&atilde;o:</strong> localhost);</li>
+                                                        <li><strong>Port</strong>: Porta do servidor (<strong>padr&atilde;o:</strong> 5432);</li>
+                                                        <li><strong>Username</strong>: Usu&aacute;rio do servidor (<strong>padr&atilde;o:</strong> postgres); e</li>
+                                                        <li><strong>Password</strong>: Senha do usu&aacute;rio do servidor (<strong>padr&atilde;o:</strong> postgres).</li>
+                                                    </ul>
+                                                </div>
+                                            </div>
+                                            <div class="thumbnail" style="border: none;">
+                                                <a name="figura_experimento_pgadmin3_add_server">
+                                                    <img src="img/experimento_pgadmin3_add_server.png" alt="Configurando o pgAdmin3">
+                                                </a>
+                                                 <legend>
+                                                    FIGURA 1 - Configurando o pgAdmin3.
+                                                </legend>
+                                            </div>
+                                            <div class="caption">
+                                                <h3>Finalizando a configura&ccedil;&atilde;o do Apache e do Experimento</h3>
+                                            </div>
+                                            <div class="caption">
+                                                <div class="texto_explicativo">
+                                                    <p>
+                                                        Para finalizar a configura&ccedil;&atilde;o do Apache e do Experimento, precisamos abrir um Terminal (atalho <kbd>ctrl+alt+T</kbd>)
+                                                        e mudar para o usu&aacute;rio root com o seguinte comando:
+                                                    </p>
+                                                    <pre>sudo su</pre>
+                                                    <p>
+                                                        Ap&oacute;s isso, precisamos criar um novo <strong>VirtualHost</strong> no apache,
+                                                        ent&atilde;o criarmos um arquivo <u>jet_medico.conf</u> em <kbd>/etc/apache2/sites-available/</kbd>,
+                                                        presionamos a tecla <kbd>I</kbd> para iniciar a edi&ccedil;&atide;o do arquivo e colocamos o seguinte conte&uacute; neste arquivo:
+                                                    </p>
+                                                    <pre class="code">&lt;VirtualHost *:80&gt;
+        # The ServerName directive sets the request scheme, hostname and port that
+        # the server uses to identify itself. This is used when creating
+        # redirection URLs. In the context of virtual hosts, the ServerName
+        # specifies what hostname must appear in the request's Host: header to
+        # match this virtual host. For the default virtual host (this file) this
+        # value is not decisive as it is used as a last resort host regardless.
+        # However, you must set it for any further virtual host explicitly.
+        ServerName jet_medico.localhost.com
+        ServerAlias jet_medico.localhost.com
+
+        ServerAdmin webmaster@localhost
+        DocumentRoot "/var/www/html/pacoteExperimento.v3/runtime-EclipseApplication/ProjetoMedicoIR/www"
+
+        # Available loglevels: trace8, ..., trace1, debug, info, notice, warn,
+        # error, crit, alert, emerg.
+        # It is also possible to configure the loglevel for particular
+        # modules, e.g.
+        #LogLevel info ssl:warn
+
+        ErrorLog ${APACHE_LOG_DIR}/error.log
+        CustomLog ${APACHE_LOG_DIR}/access.log combined
+
+        # For most configuration files from conf-available/, which are
+        # enabled or disabled at a global level, it is possible to
+        # include a line for only one particular virtual host. For example the
+        # following line enables the CGI configuration for this host only
+        # after it has been globally disabled with "a2disconf".
+        #Include conf-available/serve-cgi-bin.conf
+&lt;/VirtualHost&gt;</pre>
+                                                    <p>
+                                                        Com isso, presionamos a tecla <kbd>esc</kbd> para sair da edi&ccedil;&atilde;o e digitamos o comando <kbd>:x</kbd> para salvar o arquivo e sair do editor.
+                                                        Depois disso, digitamos o comando o seguinte comando para habilitar o <strong>VirtualHost</strong> que acabamos de criar:
+                                                    </p>
+                                                    <pre>
+                                                        <p>a2ensite jet_medico</p>
+                                                        <p>service apache2 reload</p>
+                                                    </pre>
+                                                    <p>
+                                                        Por fim, devemos editar o arquivo <strong>hosts</strong>, que est&aacute; localizado em <kbd>/etc</kbd>,
+                                                        para que possamos acessar a URL <a href="http://jet_medico.localhost.com" target="_blank">jet_medico.localhost.com</a>
+                                                        corretamente e para isso basta adicionar a seguinte linha:
+                                                    </p>
+                                                    <pre>127.0.0.1       jet_medico.localhost.com</pre>
+                                                </div>
+                                            </div>
                                         </div><!-- /container -->
                                     </div><!-- /container -->
                                 </div>
